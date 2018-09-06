a77a6ffc4a79f7a039e168ad1484123a50748940
EmilVHermansen <32634168+EmilVHermansen@users.noreply.github.com>
2018-09-06 13:16:46 +0200

    Fetch working, need to format the text

--- Fetch/fetch.js
+++ Fetch/fetch.js
@@ -1,6 +1,26 @@
+const searchButton = document.querySelector("#submitto");
+const dataField = document.querySelector("#data-field");
 var userId = document.getElementById("userId");
 
-function fetchUser(userId){
-    console.log(userId.value).innerHTML;
+searchButton.addEventListener("click", fetchUser);
 
+
+function fetchUser() {
+    var id = userId.value;
+    console.log(id);
+    
+    fetch("https://jsonplaceholder.typicode.com/users/" + id)
+        .then(function (response) {
+            if (response.status !== 200) {
+                console.log("Problem occured... Status Code: " + response.status);
+                return;
+            }
+            response.json().then(function (data) {
+                dataField.innerHTML = JSON.stringify(data);
+                console.log(JSON.stringify(data));
+            });
+        })
+        .catch(function (error) {
+            console.log("Error occured...", error);
+        })
 }
